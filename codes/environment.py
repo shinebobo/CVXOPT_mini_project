606822d8fc60176faf12b183d4490afc33087730
SSB <shinebobo12@gmail.com>
2023-12-07 13:10:20 +0900

    BGD_modify

--- codes/environment.py
+++ codes/environment.py
@@ -1,69 +1,87 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Polynomial:
+# class Polynomial:
 
-    def __init__(self, coeff):
-        self.coefficients = np.array(coeff)
+#     def __init__(self, coeff):
+#         self.coefficients = np.array(coeff)
     
-    def __call__(self, x):
-        ret = 0
-        for idx, coeff in enumerate(self.coefficients[::-1]):
-            ret += coeff*(x**idx)
-        return ret
+#     def __call__(self, x):
+#         ret = 0
+#         for idx, coeff in enumerate(self.coefficients[::-1]):
+#             ret += coeff*(x**idx)
+#         return ret
     
-    def get_grad(self, x):
-        ret = 0
-        for idx, coeff in enumerate(self.coefficients[::-1]):
-            ret += coeff*idx*(x**(idx-1))
-        return ret
+#     def get_grad(self, x):
+#         ret = 0
+#         for idx, coeff in enumerate(self.coefficients[::-1]):
+#             ret += coeff*idx*(x**(idx-1))
+#         return ret
     
-    def get_min_idx(self):
-        return -self.coefficients[1]/(2*self.coefficients[0])
+#     def get_min_idx(self):
+#         return -self.coefficients[1]/(2*self.coefficients[0])
 
     
-    
-def convex_function_generate(n):
-    ret = []
-    for _ in range(n):
-        coeff = np.random.randint(1,10, size=3)
-        ret.append(Polynomial(coeff))
-    return ret
+
+# # Random generating
+# def convex_function_generate(n):
+#     ret = []
+#     for _ in range(n):
+#         coeff = np.random.randint(1,10, size=3)
+#         ret.append(Polynomial(coeff))
+#     return ret
+
+# # Rising generating
+# def rising_function_generate(n):
+#     ret = []
+#     for i in range(n):
+#         ret.append(Polynomial([1,-i,0]))
+#     return ret
+
+#Random environmetn from a ball
+def random_square_loss_generator(d:int):
+    vec = np.random.uniform(size=d)
+    vec = vec/np.linalg.norm(vec)
+    x = np.random.uniform(size=d)
+    x = x/np.linalg.norm(x)
+    y = np.dot(vec,x)
+    y = y +np.random.normal(0,0.01)
+
+    return vec, y
+
+def project(x:np.ndarray, r):
+    if np.linalg.norm(x) > r:
+        x = x * (r / np.linalg.norm(x))
+    return x
+
+
+
+
 
 class Environment:
 
-    def __init__(self, N=50):
-        self.n = N
-        self.fts = convex_function_generate(self.n)
-        
-        # For debugging, fixed case
-        # self.fts = [Polynomial([1,6,9])]
-        # self.n=1
+    def __init__(self, d=3, T=5000):
+        self.d = d
+        self.vecs = []
+        self.labels = []
+        self.T = T
 
-        self.loss_fts = {}
+        for i in range(T):
+            vec, y = random_square_loss_generator(self.d)
+            self.vecs.append(vec)
+            self.labels.append(y)
 
-    def choose_loss_ft(self, t):
-        self.loss_fts[t] = self.fts[np.random.randint(self.n)]
+    def get_loss_val(self, t:int, x:np.ndarray):
+        assert t < self.T
+        return 0.2*(np.dot(self.vecs[t],x)-self.labels[t])**2
 
-    def get_loss_val(self, t, x):
-        assert self.loss_fts[t] != None
-        return self.loss_fts[t](x)
-    
-    def get_loss_ft(self,t):
-        assert self.loss_fts[t] != None
-        return self.loss_fts[t]
-
-    def get_regret(self,t,x):
-        assert self.loss_fts[t] != None
-        opt_x = self.loss_fts[t].get_min_idx()
-        return self.loss_fts[t](x)-self.loss_fts[t](opt_x)
+    def get_loss_grad(self, t:int,):
+        assert t < self.T
+        return self.vecs[t]
 
 
 
 
-# if __name__ == "__main__":
-    # p = Polynomial([1,-8,2])
-    # x = np.linspace(-10,10,1000)
-    # y = p(x)
-    # plt.plot(x, y)
-    # plt.savefig("ex.png")
+
+if __name__ == "__main__":
+    s = random_square_loss_generator(3)
--- codes/optimizer.py
+++ codes/optimizer.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from environment import Polynomial, Environment
+from environment import Environment, project
 from abc import ABC
 
 class Base(ABC):
@@ -8,7 +8,7 @@ class Base(ABC):
     def __init__(self, lr: float):
         self.lr = lr
     
-    def choose(self, env:Environment, x: float, t: int):
+    def choose(self, env:Environment, x: np.ndarray, t: int):
         "Choose next x"
         pass
 
@@ -16,16 +16,71 @@ class OGD(Base):
 
     def __init__(self, lr: float):
         super().__init__(lr)
+        
     
-    def choose(self, env: Environment, x: float, t: int):
-        grad = env.get_loss_ft(t).get_grad(x)
+    def choose(self, env: Environment, x: np.ndarray, t: int):
+        grad = env.get_loss_grad(t-1)
         return x - self.lr*grad
 
 class BGD_1(Base):
-    def __init__(self, lr: float):
+    def __init__(self, lr: float, pr:float, d:int):
         super().__init__(lr)
+        self.pr = pr
+        self.d = d
+        self.y = np.zeros(d)
+        
+    def choose(self, env: Environment, t: int):
+        unit_x = np.random.normal(size=env.d)
+        unit_x = unit_x/np.linalg.norm(unit_x)
+        ret = self.y + unit_x*self.pr
+        loss = env.get_loss_val(t,self.y + unit_x*self.pr)
+        grad_est = env.d/(self.pr)*loss*unit_x
+        self.y = self.y-self.lr*grad_est
+        self.y = project(self.y, 1-self.pr)
+        return ret
+
+
+class BGD_2(Base):
+    def __init__(self, lr: float, pr:float, d=int):
+        super().__init__(lr)
+        self.pr = pr
+        self.y = np.zeros(d)
+
     
-    def choose(self, env: Environment, x: float, t: int):
-        unit_x = np.random.random()
-        grad_est = (env.get_loss_val(t,x+unit_x)-env.get_loss_val(t,x-unit_x))/(2*unit_x)
-        return x-self.lr*grad_est
+    def choose(self, env: Environment, t: int):
+        unit_x = np.random.normal(size=env.d)
+        unit_x = unit_x/np.linalg.norm(unit_x)
+        ret1 = self.y + unit_x*self.pr
+        ret2 = self.y - unit_x*self.pr
+        loss1 = env.get_loss_val(t,self.y + unit_x*self.pr)
+        loss2 = env.get_loss_val(t,self.y - unit_x*self.pr)
+
+        grad_est = env.d/(2*self.pr)*(loss1-loss2)*unit_x
+        self.y = self.y-self.lr*grad_est
+        self.y = project(self.y, 1-self.pr)
+        return ret1, ret2
+
+class BGD_k(Base):
+    def __init__(self, lr: float, pr:float, d=int, k=int):
+        super().__init__(lr)
+        self.pr = pr
+        self.y = np.zeros(d)
+        self.k = k
+
+    
+    def choose(self, env: Environment, t: int):
+        unit_x = np.random.normal(size=env.d)
+        unit_x = unit_x/np.linalg.norm(unit_x)
+        delta = np.log(t+1)/(t+1)
+        ret = [self.y]
+        coords = np.eye(self.k)
+        grad_est = np.zeros(self.k)
+        
+        for i in range(self.k):
+            ret.append(self.y+delta*coords[i])
+            grad_est += (env.get_loss_val(t,self.y)-env.get_loss_val(t,self.y+delta*coords[i]))*coords[i]
+
+        grad_est = grad_est/delta
+        self.y = self.y-self.lr*grad_est
+        self.y = project(self.y, 1-self.pr)
+        return ret
